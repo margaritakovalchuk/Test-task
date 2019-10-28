43f9a37bf4da6d5494e3bc5a2cff47f6e0a315fd
margaritakovalchuk <margoshakovalchyk@gmail.com>
2019-10-28 14:00:30 +0200

    Add side bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
 const init = () => {
-    // new Slider({
-    //     slider: '.slider',
-    //     sliderItems: '.slide',
-    //     sliderBgs: '.slide__bg',
-    //     sliderControls: '.slider-control',
-    //     animationTime: 500,
-    //     slideDelay: 6000,
-    // });
-
     new MobileMenu({
         menuClass: '.main--nav .nav',
         menuButtonClass: '.button--menu',
